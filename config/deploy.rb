7ffc85dcc15e1e73ca7941463a610664b47cfde7
Julio César Domínguez Giraldo <jucedogi@ubuntu-vb.(none)>
2012-06-06 17:51:37 -0500

    revs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # INITIAL CONFIGURATION
-set :application, "comopromos.com"
+set :application, "tecnocenter.bloomweb.co"
 set :export, :remote_cache
 set :keep_releases, 5
 set :cakephp_app_path, "app"
@@ -7,22 +7,22 @@ set :cakephp_core_path, "cake"
 #default_run_options[:pty] = true # Para pedir la contraseña de la llave publica de github via consola, sino sale error de llave publica.
 
 # DEPLOYMENT DIRECTORY STRUCTURE
-set :deploy_to, "/home/comopromos/comopromos.com"
+set :deploy_to, "/home/embalao/tecnocenter.bloomweb.co"
 
 # USER & PASSWORD
-set :user, 'comopromos'
-set :password, 'c0m0pr0m0s2012'
+set :user, 'embalao'
+set :password, 'Cobr@Verde'
 
 # ROLES
-role :app, "comopromos.com"
-role :web, "comopromos.com"
-role :db, "comopromos.com", :primary => true
+role :app, "tecnocenter.bloomweb.co"
+role :web, "tecnocenter.bloomweb.co"
+role :db, "tecnocenter.bloomweb.co", :primary => true
 
 # VERSION TRACKER INFORMATION
 set :scm, :git
 set :use_sudo, false
-set :repository,  "git@github.com:bloomtec/cake_base.git"
-set :branch, "clickneat"
+set :repository,  "git@github.com:bloomtec/tecnocenter.git"
+set :branch, "master"
 
 # TASKS
 namespace :deploy do
@@ -32,12 +32,12 @@ namespace :deploy do
   task :stop do ; end
   
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "cp /home/comopromos/comopromos.com/current/. /home/comopromos/comopromos.com/ -R"
-    run "chmod 666 /home/comopromos/comopromos.com/app/config/database.php"
-    run "cp /home/comopromos/comopromos.com/app/config/database.php.srvr /home/comopromos/comopromos.com/app/config/database.php"
-    run "chmod 777 /home/comopromos/comopromos.com/app/tmp/ -R"
-    run "chmod 777 /home/comopromos/comopromos.com/app/webroot/img/uploads/ -R"
-    run "chmod 777 /home/comopromos/comopromos.com/app/webroot/files/uploads/ -R"
+    run "cp /home/embalao/tecnocenter.bloomweb.co/current/. /home/embalao/tecnocenter.bloomweb.co/ -R"
+    run "chmod 666 /home/embalao/tecnocenter.bloomweb.co/app/config/database.php"
+    run "cp /home/embalao/tecnocenter.bloomweb.co/app/config/database.php.srvr /home/embalao/tecnocenter.bloomweb.co/app/config/database.php"
+    run "chmod 777 /home/embalao/tecnocenter.bloomweb.co/app/tmp/ -R"
+    run "chmod 777 /home/embalao/tecnocenter.bloomweb.co/app/webroot/img/uploads/ -R"
+    run "chmod 777 /home/embalao/tecnocenter.bloomweb.co/app/webroot/files/uploads/ -R"
   end
   
 end
